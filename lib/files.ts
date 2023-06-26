6821bba751058394a11ab7460a86a24ea0bb1934
Ruby Iris Juric <ruby@srxl.me>
2023-06-26 17:53:16 +1000

    Update copyright notices

--- lib/files.ts
+++ lib/files.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Jacob Juric
+ * Copyright 2021-2023 Jacob Juric
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
